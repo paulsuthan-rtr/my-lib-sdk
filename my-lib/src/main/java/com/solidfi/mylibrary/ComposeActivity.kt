dd0f80b3479274315e2b8c06a93d1fe0d50a38c7
Paul Suthan <paul@solidfi.com>
2022-07-13 10:18:46 +0530

    test

--- my-lib/src/main/java/com/solidfi/mylibrary/ComposeActivity.kt
+++ my-lib/src/main/java/com/solidfi/mylibrary/ComposeActivity.kt
@@ -3,12 +3,21 @@ package com.solidfi.mylibrary
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.solidfi.mylibrary.ui.theme.DemoApplicationTheme
 
@@ -28,7 +37,21 @@ class ComposeActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            var username by remember { mutableStateOf("") }
+            TextField(
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("Label") }
+            )
+            Button(onClick = {
+
+            }) {
+
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
